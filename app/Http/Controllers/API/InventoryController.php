19031c3ccebee986b9c836a804702d553d73fd08
Kieffer Ganza <kiefferganza23@gmail.com>
2021-03-29 09:26:47 +0800

    Changed decimal places on query result

--- app/Http/Controllers/API/InventoryController.php
+++ app/Http/Controllers/API/InventoryController.php
@@ -9,13 +9,13 @@ class InventoryController extends Controller
 {
     public function index()
     {
-        $data = DB::select("SELECT c.Cutoffdate,a.recId,b.ItemName,a.quantity,isnull((a.quantity-c.Quantity)*100.0 / nullif(a.quantity,0),0) as diff
+        $data = DB::select("SELECT c.Cutoffdate,a.recId,b.ItemName,a.quantity,convert(decimal(10,2),isnull((a.quantity-c.Quantity)*100.0 / nullif(a.quantity,0),0)) as diff
         from Inventory a
-        left join item b
-        on a.ItemID = b.ID
-		left join InventoryDaily c 
-		on c.itemID = a.ItemID
-		where c.Cutoffdate = dateadd(DAY,-1,convert(date,getdate()))");
+                left join item b
+                on a.ItemID = b.ID
+                left join InventoryDaily c 
+                on c.itemID = a.ItemID
+                where c.Cutoffdate = dateadd(DAY,-1,convert(date,getdate()))");
 
         return response()->json($data);
     }
